48fbed6fd195977176eef38dbe58764151a8276f
Carolina Daza Gomez <carolina.daza@pragma.com.co>
2022-08-29 11:30:59 -0500

    continuo trabajando en la page de list implementando el componente de ionic ion-item-sliding  y ion-item-options para que al deslizar el item ya dsea a la e ionic ion-item-sliding y ion-item-options para obtener opciones al deslizar al la izquierda o derecha en mi caso opciones de favorite, share y delete tambien hicimos referencia al componente ion-list con viewChild documentacion aqui https://ionicframework.com/docs/api/item-sliding

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonList } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { DataService } from 'src/app/services/data.service';
 
@@ -8,6 +9,7 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./list.page.scss'],
 })
 export class ListPage implements OnInit {
+  @ViewChild(IonList) ionList: IonList;
   usuarios: Observable<any>;
 
   constructor(private dataService: DataService) {   }
@@ -16,6 +18,19 @@ export class ListPage implements OnInit {
     // como es un observable se debe de utilizar el subscribe() por que sino nunca se dispara el evento getUsuarios
     this.usuarios = this.dataService.getUsuario();
     this.usuarios.subscribe(console.log);
+  }
+
+  favorite(user){
+    console.log('favorite',user);
+    this.ionList.closeSlidingItems();
+  }
+  share(user){
+    console.log('user',user);
+    this.ionList.closeSlidingItems();
+  }
+  delete(user) {
+    console.log(user);
+    this.ionList.closeSlidingItems();
   }
 
 }
