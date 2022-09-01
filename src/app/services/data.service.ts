d46e22e4e4ebd85663d6ad85497bd208995b2aa4
Carolina Daza Gomez <carolina.daza@pragma.com.co>
2022-09-01 11:55:07 -0500

    Aqui configuramos todo nuestro componente de ionic llamado <ion-skeleton-text> y lo configure dentro de la page de segmento

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Componente } from '../interfaces/interfaces';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,9 @@ export class DataService {
     return this.http.get<any[]>('https://jsonplaceholder.typicode.com/albums');
   }
   getHeroes(){
-    return this.http.get<Componente[]>('/assets/data/superheroes.json');
+    return this.http.get<Componente[]>('/assets/data/superheroes.json')
+      .pipe(
+        delay( 1500)
+      );
   }
 }
