974211807d9d459eedb4caca84b9d9e6fbd095a4
Carolina Daza Gomez <carolina.daza@pragma.com.co>
2022-08-31 22:08:11 -0500

    Aqui solo hice la configuracion basica para que se muestre mi pagina mostrando mi componente personalizado header

--- src/app/pages/segment/segment.module.ts
+++ src/app/pages/segment/segment.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { SegmentPageRoutingModule } from './segment-routing.module';
 
 import { SegmentPage } from './segment.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SegmentPageRoutingModule
+    SegmentPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [SegmentPage]
 })
