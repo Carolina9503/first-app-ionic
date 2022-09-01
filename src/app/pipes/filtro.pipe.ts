6c9f8891f4a86bea2d0b553e66a63e493faaafc9
Carolina Daza Gomez <carolina.daza@pragma.com.co>
2022-08-31 21:04:10 -0500

    arregle el ultimo cambio que le hice al searh

--- src/app/pipes/filtro.pipe.ts
+++ src/app/pipes/filtro.pipe.ts
@@ -18,7 +18,7 @@ export class FiltroPipe implements PipeTransform {
     texto = texto.toLocaleLowerCase();
 
       return arreglo.filter(
-        item => item.title.toLowerCase().includes(texto)
+        item => item[coloumna].toLowerCase().includes(texto)
       );
   }
 
